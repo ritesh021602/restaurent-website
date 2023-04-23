b6ab0c40f0af39412af5f6328a9d8d7dc87018f1
ritesh021602 <75390604+ritesh021602@users.noreply.github.com>
2023-04-23 07:21:14 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./Header";
 import FoodList from "./FoodList";
+import AlertMessage from "./AlertMessage";
 function App() {
   return (
     <div>
      <Header/>
      <FoodList/>
+     <AlertMessage type="failure" message="Your order is $12" />
+
     </div>
   );
 }
--- src/FoodList.js
+++ src/FoodList.js
@@ -1,29 +1,48 @@
 import React from 'react';
-import './FoodList.css';
 
 const FoodList = () => {
-  const foodItems = [
-    { id: 1, name: 'Burger', price: 10 },
-    { id: 2, name: 'Pizza', price: 12 },
-    { id: 3, name: 'Taco', price: 6 },
-    { id: 4, name: 'Salad', price: 8 },
-  ];
-
   return (
-    <div className="food-list">
-      <h3>Our Menu</h3>
-      <ul>
-        {foodItems.map((food) => (
-          <li key={food.id}>
-            <div className="food-item">
-              <div className="food-details">
-                <h4>{food.name}</h4>
-                <p>{`$${food.price}`}</p>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        <li style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <h3>Burger</h3>
+            <div>
+              <h3>$10</h3>
+              <div>
+                <label htmlFor="item1">Amount:</label>
+                <input type="number" id="item1" name="item1" min="1" max="10" style={{marginLeft: '5px'}} />
               </div>
-              <button>Add to Cart</button>
+              <button>Add to cart</button>
             </div>
-          </li>
-        ))}
+          </div>
+        </li>
+        <li style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <h3>Pizza</h3>
+            <div>
+              <h3>$12</h3>
+              <div>
+                <label htmlFor="item2">Amount:</label>
+                <input type="number" id="item2" name="item2" min="1" max="10" style={{marginLeft: '5px'}} />
+              </div>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        </li>
+        <li style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <h3>Sandwich</h3>
+            <div>
+              <h3>$15</h3>
+              <div>
+                <label htmlFor="item3">Amount:</label>
+                <input type="number" id="item3" name="item3" min="1" max="10" style={{marginLeft: '5px'}} />
+              </div>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   );
